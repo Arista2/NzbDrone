746bddf13aea6a697c211bcc752a2d3b29d54a67
Mark McDowall <markus.mcd5@gmail.com>
2012-09-02 22:10:42 -0700

    Store status

--- NzbDrone.Services/NzbDrone.Services.Service/Migrations/Migration20120831.cs
+++ NzbDrone.Services/NzbDrone.Services.Service/Migrations/Migration20120831.cs
@@ -11,10 +11,11 @@ namespace NzbDrone.Services.Service.Migrations
         public override void Up()
         {
             Database.AddTable("IdMappings", new Column("TvDbId", DbType.Int32, ColumnProperty.PrimaryKey),
-                                        new Column("TvDbTitle", DbType.String, ColumnProperty.NotNull),
-                                        new Column("TvRageId", DbType.Int32, ColumnProperty.NotNull),
-                                        new Column("TvRageTitle", DbType.String, ColumnProperty.NotNull),
-                                        new Column("FirstAired", DbType.DateTime, ColumnProperty.NotNull));
+                                            new Column("TvDbTitle", DbType.String, ColumnProperty.NotNull),
+                                            new Column("TvRageId", DbType.Int32, ColumnProperty.NotNull),
+                                            new Column("TvRageTitle", DbType.String, ColumnProperty.NotNull),
+                                            new Column("FirstAired", DbType.DateTime, ColumnProperty.NotNull),
+                                            new Column("Status", DbType.String, ColumnProperty.NotNull));
         }
 
         public override void Down()
